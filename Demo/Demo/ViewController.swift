baf41b58c8cd062988ac497ae4d8cc0e1fa0d109
Chengyu <1529514910@qq.com>
2017-07-08 18:49:08 -0700

    new version

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "batman_chat_bg.png")!)
+        
         robot.image = UIImage(named: MyGlobal.selectedImg)
         self.navigationController?.isNavigationBarHidden = true;
         if self.revealViewController() != nil {
